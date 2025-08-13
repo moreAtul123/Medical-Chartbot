7204e10afc4b312af292c3aa76d28f00e56ea9ad
Atul More <atulmor@amdocs.com>
2025-08-13 14:50:58 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+# Create directory 
+mkdir -p src
+mkdir -p research
+# Create file 
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompty.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch README.md
+touch requirements.txt
+
+
+
+
+echo "Directory and file structure created successfully."
